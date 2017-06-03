c083b05245797c2bb333cd0eee63f2d57a0f44b6
Brad Menchl <brmenchl@gmail.com>
2017-06-03 18:42:17 -0400

    Switch to axios middleware because api middleware is currently broken

--- src/store.js
+++ src/store.js
@@ -1,23 +1,33 @@
 import {createStore, applyMiddleware} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import {apiMiddleware} from 'redux-api-middleware';
+import axios from 'axios';
+import axiosMiddleware from 'redux-axios-middleware';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import reducer from './reducers';
 
 const initialState = {};
+const client = axios.create({
+  responseType: 'json'
+});
 
-const baseMiddleware = [thunk, apiMiddleware];
+const baseMiddleware = [thunk, axiosMiddleware(client)];
 
-const enhancer =
-    process.env.NODE_ENV === 'production'
-    ? applyMiddleware(...baseMiddleware)
-    : composeWithDevTools(
+let enhancer;
+
+if (process.env.NODE_ENV === 'production') {
+    enhancer = applyMiddleware(...baseMiddleware)
+}
+
+else {
+    const {composeWithDevTools} = require('redux-devtools-extension');
+    const logger = require('redux-logger').default;
+    
+    enhancer = composeWithDevTools(
         applyMiddleware(
             ...baseMiddleware,
             logger
         )
     );
+}
 
 export default createStore(
     reducer,
