5cf9f5ba4050a70203b168458b56a428f7cd0d0b
Brad Menchl <brad.menchl@pelotoncycle.com>
2017-12-02 18:13:17 -0500

    Set up mock data with sls

--- client/src/audioAnalysis/audioAnalysisGateway.js
+++ client/src/audioAnalysis/audioAnalysisGateway.js
@@ -1,23 +1,33 @@
 import request from 'superagent';
 
-const URL = 'https://git.heroku.com/audio-moodring-api-interface.git/notes';
+const URL = (process.env.NODE_ENV === 'development')
+  ? 'http://localhost:3001/mood'
+  : 'https://gwkxo1ko5b.execute-api.us-east-1.amazonaws.com/dev/mood';
+
 const MAX_MIDI = 127;
 
 export const analyzeFile = file =>
   new Promise((resolve, reject) =>
-    request
-      .post(URL)
-      .field('detailed_result', true)
-      .field('access_id', process.env.REACT_APP_SONIC_API)
-      .field('format', 'json')
-      .attach('input_file', new File(file, 'input.wav'))
+    request.get(URL)
       .end((error, response) =>
         error
           ? reject(error)
-          : resolve(toNotes(response.body))));
+          : resolve(toNotes(response.body))
+      ));
+// new Promise((resolve, reject) =>
+//   request
+//     .post(URL)
+//     .field('detailed_result', true)
+//     .field('access_id', process.env.REACT_APP_SONIC_API)
+//     .field('format', 'json')
+//     .attach('input_file', new File(file, 'input.wav'))
+//     .end((error, response) =>
+//       error
+//         ? reject(error)
+//         : resolve(toNotes(response.body))));
 
-const toNotes = (response) =>
-  response.melody_result.pitch_curve.map(note => ({
+const toNotes = ({melody}) =>
+  melody.melody_result.pitch_curve.map(note => ({
     pitch: note.midi_pitch / MAX_MIDI,
     startTime: note.time,
     volume: note.volume
--- server/handler.js
+++ server/handler.js
@@ -1,12 +1,15 @@
 'use strict';
 
 module.exports.mood = (event, context, callback) => {
+  const mockJsonCombined = require('./mockJsonCombined.json');
+  mockJsonCombined.melody = require('./melody.json');
+
   const response = {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*', // Required for CORS support to work
     },
-    body: JSON.stringify(require('./mockJsonCombined.json')),
+    body: JSON.stringify(mockJsonCombined),
   };
 
   callback(null, response);
