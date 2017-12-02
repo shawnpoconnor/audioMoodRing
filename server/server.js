6e846c0e884497a952288baae3cdb92e59c9d414
Shawn O'Connor <bartandfosto@gmail.com>
2017-12-02 16:35:26 -0500

    add combined mock file, remove split json, and update server to use combined mock return and client to make call to heroku api.

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
 
+
 app.listen(8000, function() {
   console.log('listening on 8000')
 })
 
 app.post('/notes', function(req, res) {
-  //some kind of API call
-  res.sendFile(__dirname + '/mockJsonNotes.json')
-})
+  //some kind of speechToText API call
+  //some kind of audioAnalysis API call
 
-app.post('/words', function(req, res) {
-  //some kind of API call
-  res.sendFile(__dirname + '/mockJsonWords')
+  //make a response var that combines both json objects
+
+  // console.log('Hit API route!')
+  res.sendFile(__dirname + '/mockJsonCombined.json')
 })
